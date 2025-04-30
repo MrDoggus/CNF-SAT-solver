477a0ea25e9d572a4af8cfad7b8b2c07a63d74c2
James DeGruccio <kielianoftarney@gmail.com>
2025-04-30 19:54:20 -0400

    renamed new_SAT_solver to mySAT, deleted old mySAT

--- mySAT.py
+++ mySAT.py
@@ -1,21 +1,100 @@
 import sys
+import time
+import glob
+from dpll import *
 from SAT_structs import *
 from SAT_solver import SAT_solver
 
 if __name__ == "__main__":
-    cnf_file = sys.argv[1]
-
-    formula = CNF_Formula.from_dimacs_file(cnf_file)
+    total_iterations = 0
+    successful_files = 0
+    total_processing_time = 0
+    file_count = 0
+    useDPLL = False
+    useCDCL = False
+    useVSIDS = False
+    debug = False
     
-    solver = SAT_solver(formula)
-    assignl = solver.solve()
-    if (assignl is None):
-        print("RESULT:UNSAT")
-    else: 
-        print("RESULT:SAT")
-        assignStr = "ASSIGNMENT:" + " ".join([f"{i}={"1" if assignl[i] else "0"}" for i in range(1,len(assignl))])
-        print(assignStr)
+    if len(sys.argv) < 2:
+        print("Usage: python new_SAT_solver.py [-dpll | -vsids | -cdcl] *.cnf")
+        sys.exit(1)
 
-    # print(assignl)
-    # print(solver.iter_count)
+    #cnf_files = sys.argv[1:]
+    #cnf_files = sys.argv[(len(sys.argv)-1):]
+    
+    args = sys.argv[1:]
+    cnf_files = []
+    for arg in args:
+        if arg == "-dpll":
+            useDPLL = True
+        elif arg == "-cdcl":
+            useCDCL = True
+        elif arg == "-vsids":
+            useVSIDS = True
+        elif arg == "-debug":
+            debug = True
+        else:
+            cnf_files.append(arg)
+
+    if (not useDPLL and not useCDCL and not useVSIDS):
+        useVSIDS = True
+
+    if len(cnf_files) == 1:
+        cnf_files = glob.glob(cnf_files[0])
+
+    if not cnf_files:
+        print("No CNF files provided.")
+        sys.exit(1)
+
+    for cnf_file in cnf_files:
+        if (debug):
+            print("\n")
+        try:
+            start_time = time.time()
         
+            if useDPLL == True:
+                if (debug): 
+                    print(f"--- Solving {cnf_file} using DPLL ---")
+                clauses = parse_dimacs_file(cnf_file)
+                (solution, iter_count) = dpll(clauses, {}, 0)
+            else:
+                formula = CNF_Formula.from_dimacs_file(cnf_file)
+                solver = SAT_solver(formula, log=debug)
+                if useVSIDS == True:
+                    if (debug):
+                        print(f"--- Solving {cnf_file} using CDCL w/ VSIDS ---")
+                    solution = solver.solve(useVSIDS=True)
+                    iter_count = solver.iter_count
+                elif useCDCL == True:
+                    if (debug): 
+                        print(f"--- Solving {cnf_file} using CDCL ---")
+                    solution = solver.solve()  # Default useVSIDS=False
+                    iter_count = solver.iter_count
+
+            end_time = time.time()
+            single_file_processing_time = end_time - start_time
+            
+            if (solution is None):
+                print("RESULT:UNSAT")
+            else: 
+                print("RESULT:SAT")
+                assignStr = "ASSIGNMENT:" + " ".join([f"{i}={"1" if solution[i] else "0"}" for i in range(1,len(solution))])
+                print(assignStr)
+
+            if (debug):
+                print(f"Number of iterations: {iter_count:.2f}")
+                print(f"Processing time: {single_file_processing_time:.4f} seconds")
+                
+            total_iterations += iter_count
+            file_count += 1
+            total_processing_time += single_file_processing_time
+        except Exception as e:
+            print(f"Failed to parse {cnf_file}: {e}")
+            continue  # Skip broken files
+
+
+    if (debug):
+        avg_iter_count = total_iterations / file_count
+        print("\n==============================")
+        print(f"Total Processing time: {total_processing_time:.4f} seconds")
+        print(f"Average number of iterations over {file_count} successful files: {avg_iter_count:.2f}")
